05e9e744821930cd38e25f576535e7e998a906e4
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-16 18:57:11 +0530

    blog pages added

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const blog = () => {
   return (
-    <div>blog</div>
+    <div className={styles.main}>
+      <div className={styles.blogs}>
+        <h2>Popular Blogs</h2>
+        <div className={styles.blogItem}>
+          <div className='myimg'><Image className={styles.IMG} src='/coder.jpg' alt='' width={320} height={213} priority={false} /></div>
+          <Link href={'/blogpost/Learn-Js'}>
+            <h3>How to learn JavaScript in 2022?</h3>
+          </Link>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2022?</h3>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2022?</h3>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '@/styles/blog.module.css'
 
 const slug = () => {
     const router = useRouter()
     console.log(router.query)
     const { slug } = router.query
     return (
-        <div>{slug}</div>
+        <div className={styles.blog}>
+            <h1>
+                Title of the page {slug}
+            </h1>
+            <hr />
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum id voluptatibus accusantium sit modi labore quaerat officia facilis iusto dicta, explicabo porro dolore, distinctio enim assumenda repellat unde corrupti eius minima earum dolores eum reiciendis minus! Blanditiis laborum officiis accusamus.</div>
+        </div>
     )
 }
 
