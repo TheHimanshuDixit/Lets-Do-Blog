f40a9d2ac9b4ed2da4b1b6d1937f7b751bf92379
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-17 17:53:13 +0530

    server site and static site generation rendering

--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,25 @@
 import React, { useEffect } from 'react'
 import styles from '@/styles/Home.module.css'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import * as fs from 'fs';
 
 // Step 1: Collect all the files from blogdara folder
 // Step 2: Loop over the files and create a blog post for each
 
-const blog = () => {
-  const [blogs, setBlogs] = useState([])
+const blog = (props) => {
+  const [blogs, setBlogs] = useState(props.allblogs)
 
-  useEffect(() => {
-    console.log('useEffect is running ');
-    fetch('http://localhost:3000/api/blogs').then((a) => {
-      return a.json();
-    }).then((b) => {
-      console.log(b);
-      setBlogs(b);
-    })
-  }, [])
+  // useEffect(() => {
+  //   console.log('useEffect is running ');
+  //   fetch('http://localhost:3000/api/blogs').then((a) => {
+  //     return a.json();
+  //   }).then((b) => {
+  //     console.log(b);
+  //     setBlogs(b);
+  //   })
+  // }, [])
 
   return (
     <div className={styles.main}>
@@ -26,7 +27,8 @@ const blog = () => {
         <h2>Popular Blogs</h2>
         {blogs.map((blogitem) => {
           return <div className={styles.blogItem} key={blogitem.slug}>
-            <div className='myimg'><Image className={styles.IMG} src='/coder.jpg' alt='' width={320} height={213} priority={true} /></div>
+            {/* <div className='myimg'><Image className={styles.IMG} src='/coder.jpg' alt='' width={320} height={213} priority={true} /></div> */}
+            <div className='myimg'><img className={styles.IMG} src='/coder.jpg' alt='' width={320} height={213}/></div>
             <Link href={'/blogpost/' + blogitem.slug}>
               <h3>{blogitem.title}</h3>
             </Link>
@@ -38,5 +40,39 @@ const blog = () => {
     </div>
   )
 }
+
+// server side rendering
+
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const data = await fetch(`http://localhost:3000/api/blogs`)
+//   const allblogs = await data.json()
+
+//   // Pass data to the page via props
+//   return { props: { allblogs } }
+// }
+
+
+// static side rendering 
+
+export async function getStaticProps() {
+  let data = await fs.promises.readdir("blogdata")
+  let myfile;
+  let allblogs = [];
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    myfile = await fs.promises.readFile(("blogdata/" + item), "utf-8")
+    allblogs.push(JSON.parse(myfile));
+  }
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      allblogs
+    }
+  }
+}
+
 
 export default blog
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from '@/styles/blog.module.css'
+import * as fs from 'fs';
 
 // Step 1: Find the right blog post based on the slug
 // Step 2: Render the blog post
 
-const slug = () => {
-    const [blog, setBlog] = useState([])
-    const router = useRouter()
+const Slug = (props) => {
+    const [blog, setBlog] = useState(props.myblog)
+    // const router = useRouter()
 
-    useEffect(() => {
-        if (!router.isReady)
-            return;
+    // useEffect(() => {
+    //     if (!router.isReady)
+    //         return;
 
-        const { slug } = router.query
-        console.log('useEffect is running ');
-        fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a) => {
-            return a.json();
-        }).then((b) => {
-            console.log(b);
-            setBlog(b);
-        })
-    }, [router.isReady])
+    //     const { slug } = router.query
+    //     console.log('useEffect is running ');
+    //     fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a) => {
+    //         return a.json();
+    //     }).then((b) => {
+    //         console.log(b);
+    //         setBlog(b);
+    //     })
+    // }, [router.isReady])
     return (
         <div className={styles.blog}>
             <h1>
@@ -33,4 +34,42 @@ const slug = () => {
     )
 }
 
-export default slug
+
+// export async function getServerSideProps(context) {
+//     // Fetch data from external API
+//     const { slug } = context.query
+//     const data = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`)
+//     const myblog = await data.json()
+
+//     // Pass data to the page via props
+//     return { props: { myblog } }
+// }
+
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { slug: 'how-to-learn-javascript' } },
+            { params: { slug: 'how-to-learn-nodejs' } },
+            { params: { slug: 'how-to-learn-nextjs' } },
+        ],
+        fallback: true,
+    };
+}   
+
+export async function getStaticProps(context) {
+    console.log(context)
+    const { slug } = context.params;
+    let myblog = await fs.promises.readFile(`blogdata/${slug}.json`, 'utf-8')
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            myblog: JSON.parse(myblog)
+        },
+    }
+}
+
+
+export default Slug
