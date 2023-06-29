70ecf304ff39a5d4dbbb7d9bbbab2124877274f9
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-29 19:32:28 +0530

    tailwind css added

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '@/styles/globals.css'
 // all page start from here 
 
 export default function App({ Component, pageProps }) {
-  console.log('app');
+  // console.log('app');
   return <>
     <Navbar />
     <Component {...pageProps} />
